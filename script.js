0d7d6a140a294608358f7191ef9f5bd35eef057a
Funni <raptureorb@gmail.com>
2025-06-09 14:19:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ window.onload = () => {
   container.innerHTML = "<p>Checking server statuses...</p>";
 
   const versions = [
-    { year: "2016", url: "http://localhost:2016/status" },
-    { year: "2017", url: "http://localhost:2017/status" },
-    { year: "2018", url: "http://localhost:2018/status" },
-    { year: "2019", url: "http://localhost:2019/status" },
-    { year: "2020", url: "http://localhost:2020/status" },
+    { year: "2016", url: "http://localhost:2056/status" },
+    { year: "2017", url: "http://localhost:2056/status" },
+    { year: "2018", url: "http://localhost:2056/status" },
+    { year: "2019", url: "http://localhost:2056/status" },
+    { year: "2020", url: "http://localhost:2056/status" },
     { year: "2021", url: "https://oldrecapi.biotest.dev/status" }
   ];
 
